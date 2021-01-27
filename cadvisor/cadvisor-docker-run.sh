7003d34e257aa12e36d14cf7ba421905a19592be
Roberto Pozzi <r.robipozzi@gmail.com>
2021-01-27 12:13:22 +0100

    fix cAdvisor issue

--- cadvisor/cadvisor-docker-run.sh
+++ cadvisor/cadvisor-docker-run.sh
@@ -14,8 +14,8 @@ echo ${cyn}Docker network created${end}
 echo
 echo ${cyn}Running $CADVISOR_CONTAINER_NAME container ...${end}
 # Run CADVISOR mounting configuration file as a volume
-#CMD_RUN="docker run --name $CADVISOR_CONTAINER_NAME --network $MONITORING_NETWORK -d -p 8081:8080 -v /:/rootfs:ro -v /var/run:/var/run:rw -v /sys:/sys:ro -v /var/lib/docker/:/var/lib/docker:ro $CADVISOR_CONTAINER_IMAGE_NAME:$CADVISOR_CONTAINER_IMAGE_VERSION -url_base_prefix=/cadvisor"
-CMD_RUN="docker run --name $CADVISOR_CONTAINER_NAME --network $MONITORING_NETWORK -d -v /:/rootfs:ro -v /var/run:/var/run:rw -v /sys:/sys:ro -v /var/lib/docker/:/var/lib/docker:ro $CADVISOR_CONTAINER_IMAGE_NAME:$CADVISOR_CONTAINER_IMAGE_VERSION -url_base_prefix=/cadvisor"
+#CMD_RUN="docker run --name $CADVISOR_CONTAINER_NAME --network $MONITORING_NETWORK -d -p 8081:8080 -v /:/rootfs:ro -v /var/run/docker.sock:/var/run/docker.sock:rw -v /sys:/sys:ro -v /var/lib/docker/:/var/lib/docker:ro $CADVISOR_CONTAINER_IMAGE_NAME:$CADVISOR_CONTAINER_IMAGE_VERSION -url_base_prefix=/cadvisor"
+CMD_RUN="docker run --name $CADVISOR_CONTAINER_NAME --network $MONITORING_NETWORK -d -v /:/rootfs:ro -v /var/run/docker.sock:/var/run/docker.sock:rw -v /sys:/sys:ro -v /var/lib/docker/:/var/lib/docker:ro $CADVISOR_CONTAINER_IMAGE_NAME:$CADVISOR_CONTAINER_IMAGE_VERSION -url_base_prefix=/cadvisor"
 echo ${cyn}Running Docker container with:${end} ${grn}$CMD_RUN${end}
 $CMD_RUN
 # Connect to Docker Network inspect
